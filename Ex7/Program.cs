f58c4e8f06d0b55d4498d2320a4a45089e37b955
SergyVR <rsv1177@gmail.com>
2022-10-08 02:10:44 +0300

    домашка

---
+++ Ex7/Program.cs
@@ -0,0 +1,12 @@
+﻿// выводим чётные числа до заданного
+
+   Console.WriteLine("Введите число"); 
+ 
+  string firstNumber = Console.ReadLine();
+  int Num = int.Parse(firstNumber); 
+  int nk = 2;
+   while (Num > nk)      
+   {
+      Console.WriteLine(nk);
+       nk = nk + 2; 
+   }
